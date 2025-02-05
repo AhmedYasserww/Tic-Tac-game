367e9399658bce9f37912aab7d6cbe87b8e43529
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2025-02-05 21:46:43 +0200

    finish the auto play

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -44,34 +44,37 @@ body: SafeArea(
            // childAspectRatio: 1,
               crossAxisSpacing: 8,
           mainAxisSpacing: 8,
-padding: const EdgeInsets.all(20),
-        crossAxisCount: 3,
-        children: List.generate(9, (index) {
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: InkWell(
-               onTap:() =>  gameOver ? null : () => onTapped(index),
+          padding: const EdgeInsets.all(20),
+                  crossAxisCount: 3,
+                  children: List.generate(9, (index) {
+          return InkWell(
+            // onTap:() =>  gameOver ? null : () => onTapped(index),
+              onTap:() =>onTapped(index),
 
 
-
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xff001456),
-                 borderRadius: BorderRadius.circular(20),
-                 // border: Border.all(color: Colors.grey),
-                ),
-                child: Center(
-                  child: Text(
-                    "x",
-                   // game.board[index],
-                    style: TextStyle(color: Colors.blue, fontSize: 52),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff001456),
+               borderRadius: BorderRadius.circular(20),
+               // border: Border.all(color: Colors.grey),
+              ),
+              child: Center(
+                child: Text(
+                  Player.playerX.contains(index) ? "X" :
+                  Player.playerO.contains(index) ? "0" :
+                  " ",
+                 // game.board[index],
+                  style:
+                   TextStyle(
+                      color:  Player.playerX.contains(index) ? Colors.blue :
+                      Colors.pink, fontSize: 52
                   ),
                 ),
               ),
             ),
           );
-        }),
-      )),
+                  }),
+                )),
       Text(
         result,
         style: const TextStyle(
@@ -85,6 +88,8 @@ padding: const EdgeInsets.all(20),
         color: const Color(0xff4169e8),
         onPressed: (){
           setState(() {
+            Player.playerO.clear();
+            Player.playerX.clear();
              activePlayer = 'X';
              gameOver = false;
             isSwitched = false;
@@ -101,6 +106,25 @@ padding: const EdgeInsets.all(20),
     );
   }
 
-  onTapped(int index) {}
+  onTapped(int index) async{
+
+    if((Player.playerX.isEmpty || !Player.playerX.contains(index)) &&
+        (Player.playerO.isEmpty || !Player.playerO.contains(index))){
+      game.playGame(index, activePlayer);
+      updateState();
+      if(!isSwitched && !gameOver){
+        await game.autoPlay(activePlayer);
+        updateState();
+      }
+
+    }
+
+  }
+
+  void updateState() {
+     setState(() {
+      activePlayer = (activePlayer == 'X') ? 'O' : 'X';
+    });
+  }
   //game.playGame(index) {}
 }
--- lib/logic/game_logic.dart
+++ lib/logic/game_logic.dart
@@ -1,13 +1,47 @@
+
+import 'dart:math';
+
 class Player {
 static const x = 'X';
 static const o = 'O';
 static const empty = '';
 static List<int>playerX =[];
-static List <int>playerO = [];
+static List<int>playerO =[];
 }
 
+extension ContainsAll on List {
+  bool containsAll(int x , int y , int z){
+    return (contains(x) && contains(y) && contains(z));
+  }
+}
 class Game {
   void playGame(int index, String activePlayer) {
+    if(activePlayer =='X'){
+     Player.playerX.add(index);
+    }
+    else{
+      Player.playerO.add(index);
+    }
+  }
+  checkWinner(){
+    String winner = '';
+    if(Player.playerX.containsAll(0,1,2)){
+      return winner;
+    }
+
+  }
+  Future<void> autoPlay(activePlayer)async{
+    int index = 0;
+    List<int>emptyCells = [];
+    for(int i=0;i<9;i++){
+      if(!Player.playerX.contains(i) && !Player.playerO.contains(i)){
+        emptyCells.add(i);
+      }
+    }
+    Random random = Random();
+    int randomIndex = random.nextInt(emptyCells.length);
+    index = emptyCells[randomIndex];
+    playGame(index, activePlayer);
   }
 
 }
